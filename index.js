1bfc03a5582f1ae0cbde1cbc65f108624e69bbc4
Cassio-Ares <137903978+Cassio-Ares@users.noreply.github.com>
2023-12-25 19:48:41 -0300

    basico de rotas

--- index.js
+++ index.js
@@ -3,6 +3,43 @@ const app = express();
 
 app.use(express.json());
 
+/**
+ * vamos criar as rotas aqui no index mas normalmente se cria as rotas em uma 
+ * pasta chamada routes
+ */
+
+
+/**
+ * Estrutura basica de uma rota:
+ * 
+ * app.get('/', function(req, res){
+ *  return res ou req . status ()....
+ * }
+ * 
+ * app (chama o express). get(aqui citamos o verbo de ação get, post, delet, put)
+ * ('/ e o nome da rota', função (res(resposta), req(requisição)) =>{
+ *    return(toda função retorna algo) este retorno tem um status(). uma forma de comunicação ex: json
+ * })
+ * 
+ */
+app.get('/usuarios', (req, res)=>{
+    return res.status(200).json([]);
+});
+
+app.post('/usuarios', (req, res)=>{
+    return res.status(200).json([]);
+});
+
+app.get('/noticias', (req, res)=>{
+    return res.status(200).json([]);
+});
+
+app.post('/noticias', (req, res)=>{
+    return res.status(200).json([]);
+})
+
+
+
 app.listen(8080, ()=>{              
     console.log("Servidor rodando na porta 8080")             
 });
--- src/modules/noticia.model.js
+++ src/modules/noticia.model.js
@@ -2,11 +2,6 @@ import mongoose from "mongoose";
 
 const { Schema } = mongoose;
 
-/**
- * schema sistema que determina as colunas de uma tabela 
- * (aqui por ex temos a coluna nome, email, senha ); ao determinar uma coluna temos que dizer 
- * qual modelo de dado que ela irá trabalhar ex: string, int, date, ...
- */
 
 const noticiaSchema = new Schema(
     {
@@ -19,14 +14,7 @@ const noticiaSchema = new Schema(
     }
     );
 
-      /**
-     *  const UsuarioModel = mongoose.model('usuario', usuarioSchema);
-     * 
-     * UsuarioModel irá carregar o schema ('usuario'/ dentro dos parenteses é o nome que a tabela terá 
-     * no banco de dados/, usuarioSchema é a estrutura )
-     * 
-     */
-
+    
 const NoticiaModel = mongoose.model('noticias', noticiaSchema);
 
 export default NoticiaModel;
